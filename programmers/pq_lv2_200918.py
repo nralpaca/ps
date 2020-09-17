b39bd77596ffb6b763861723516cb63af3099d9c
unknown <nralpaca@gmail.com>
2020-09-18 01:20:25 +0900

    priority queue

---
+++ programmers/pq_lv2_200918.py
@@ -0,0 +1,24 @@
+from queue import PriorityQueue
+
+def solution(scoville, K):
+    answer = 0
+
+    pq = PriorityQueue()
+
+    for scov in scoville:
+        pq.put(scov)
+
+    while True:
+        first = pq.get()
+        if first >= K:
+            break
+
+        second = pq.get()
+
+        scov = first + second * 2
+        answer += 1
+        pq.put(scov) 
+
+    return answer
+
+#print(solution([1, 2, 3, 9, 10, 12], 7))
